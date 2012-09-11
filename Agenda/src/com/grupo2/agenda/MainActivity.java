57a8ba9c5091d2e4aea9c9862bc8c6ae6ce5fdca
Rafael <jrtadf@gmail.com>
2012-09-11 02:11:18 -0300

    Adicionado aplicação inicial em phonegap + jquery mobile

--- Agenda/src/com/grupo2/agenda/MainActivity.java
+++ Agenda/src/com/grupo2/agenda/MainActivity.java
@@ -5,20 +5,22 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
+import org.apache.cordova.*;
 
-public class MainActivity extends Activity {
+public class MainActivity extends DroidGap {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        super.loadUrl("file:///android_asset/www/app.html");
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+    //@Override
+    //public boolean onCreateOptionsMenu(Menu menu) {
+    //    getMenuInflater().inflate(R.menu.activity_main, menu);
+    //    return true;
+    //}
 
     
 }
