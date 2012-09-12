7c45a597b97131792b5d50a02eb8f25dafa6b746
Rafael <jrtadf@gmail.com>
2012-09-12 01:07:56 -0300

    Adicionado template list soap Adicionada requisição list

--- Agenda/assets/www/js/script.js
+++ Agenda/assets/www/js/script.js
@@ -1,31 +1,37 @@
-var webserviceUrl = "http://127.0.0.1:8000/AgendaTelefonica/PhonebookService";
+$( document ).bind( "mobileinit", function() {
+    // Make your jQuery Mobile framework configuration changes here!
+    $.support.cors = true;
+    $.mobile.allowCrossDomainPages = true;
+});
+
+var webserviceUrl = "http://127.0.0.1:8080/AgendaTelefonica-JSP/PhonebookService";
 
 $(function() {
-                $('a#botao-adicionar').click(function(){
-                    $("ul#contatos").append("<li data-theme='c'><a href='#page1' data-transition='slide'>Noob!</a></li>");
-                    $('ul#contatos').listview('refresh');
+    $('a#botao-adicionar').click(function(){
+        $("ul#contatos").append("<li data-theme='c'><a href='#page1' data-transition='slide'>Noob!</a></li>");
+        $('ul#contatos').listview('refresh');
+                    
+        console.log($('script#soap-template-list').html());
+        $.ajax({
+            type: "post",
+            url: webserviceUrl,
+            contentType: "text/xml",
+            data: $('script#soap-template-list').html(),
+            dataType: "xml",
+            processData: false,
+            sucess: function(data){
+                console.log(data);
+            },
+            error: function(){
+                console.log('erro');
+            }
+        });
                     
                     
-                    $.ajax({
-                    	type: "post",
-                    	url: webserviceUrl,
-                    	contentType: "text/xml",
-                    	//data: soapBody,
-                    	dataType: "xml",
-                    	processData: false,
-                    	sucess: function(data){
-                    		console.log(data);
-                    	},
-                    	error: function(){
-                    		console.log('erro');
-                    	}
-                    });
-                    
-                    
-                    
-                    
-                });
+    });
 });
+
+
 
 
 
