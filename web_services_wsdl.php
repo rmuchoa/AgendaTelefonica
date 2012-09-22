ae7295271bbbf396e64c4704af728abacc1717cf
Rafael <jrtadf@gmail.com>
2012-09-22 17:13:12 -0300

    descrição wsdl

---
+++ web_services_wsdl.php
@@ -0,0 +1,8 @@
+if (isset($_GET['wsdl'])) {
+    $autodiscover = new Zend_Soap_AutoDiscover();
+    $autodiscover->setClass('Application_Model_WebServices');
+    $autodiscover->setBindingStyle(array('style' => 'rpc'));
+    $autodiscover->handle();
+} else {
+   ...
+}
